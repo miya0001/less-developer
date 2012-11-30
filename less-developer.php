811bd4e74704628df6c1187a338921c0acd29965
miya <miya@wpist.me>
2012-11-30 15:22:11 +0900

    first commit

---
+++ less-developer.php
@@ -0,0 +1,52 @@
+<?php
+/*
+Plugin Name: Less Developer
+Description: Auto change *.css to *.less for theme development.
+Author: Takayuki Miyauchi
+Plugin URI: http://wpist.me/
+Version: 0.1.0
+Author URI: http://wpist.me/
+Domain Path: /languages
+Text Domain: less-developer
+*/
+
+new LessDeveloper();
+
+class LessDeveloper {
+
+function __construct()
+{
+    add_filter('style_loader_tag', array(&$this, 'style_loader_tag'), 5, 2);
+    add_filter('wp_enqueue_scripts', array(&$this, 'wp_enqueue_scripts'));
+}
+
+public function style_loader_tag($tag, $handler)
+{
+    global $wp_styles;
+    $style = $wp_styles->registered[$handler];
+    $src = $style->src;
+    $less = preg_replace('/\.css$/i', '.less', $src);
+
+    $res = wp_remote_head($less);
+
+    if (!is_wp_error($res) && $res['response']['code'] === 200) {
+        $tag = str_replace($src, $less, $tag);
+        $tag = preg_replace("/stylesheet/i", 'stylesheet/less', $tag);
+    }
+
+    return $tag;
+}
+
+public function wp_enqueue_scripts()
+{
+    wp_enqueue_script(
+        'lesscss',
+        'http://cloud.github.com/downloads/cloudhead/less.js/less-1.3.1.min.js',
+        array(),
+        null,
+        false
+    );
+}
+
+}
+
---
+++ readme.txt
@@ -0,0 +1,27 @@
+=== less developer ===
+Contributors: miyauchi
+Donate link: http://wpist.me/
+Tags: less, css
+Requires at least: 3.4
+Tested up to: 3.5
+Stable tag: 0.1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Switch *.css to *.less automatically if your theme using less framework.
+
+== Description ==
+
+[This plugin maintained on GitHub.](https://github.com/miya0001/hr-widget)
+
+
+== Installation ==
+
+* A plug-in installation screen is displayed on the WordPress admin panel.
+* It installs it in `wp-content/plugins`.
+* The plug-in is made effective.
+
+== Changelog ==
+
+= 0.1.0 =
+* The first release.
